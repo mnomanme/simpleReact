7495dc5cd4c69af010d528dae8ae24a8ffd7283c
nomanme <abdullah.noman083@gmail.com>
2021-09-06 23:52:08 +0600

    movie counter component set

--- 33-how-react-works-recap-react-concepts/recap-react-concepts/src/Layout.js
+++ 33-how-react-works-recap-react-concepts/recap-react-concepts/src/Layout.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import MovieCounter from './MovieCounter';
 import Nayok from './Nayok';
 
 const Layout = () => {
 	const nayoks = ['Sohel', 'Salman', 'Bapparaj'];
 	return (
 		<div>
-			<Nayok name={nayoks[0]} movie="Pirates of Carabian"></Nayok>
-			<Nayok name="Shakib" age="5"></Nayok>
-			<Nayok name={nayoks[2]} age="20"></Nayok>
+			<section className="text-center">
+				<MovieCounter></MovieCounter>
+			</section>
+			<section>
+				<Nayok name={nayoks[0]} movie="Pirates of Carabian"></Nayok>
+				<Nayok name="Shakib" age="5"></Nayok>
+				<Nayok name={nayoks[2]} age="20"></Nayok>
+			</section>
 		</div>
 	);
 };
