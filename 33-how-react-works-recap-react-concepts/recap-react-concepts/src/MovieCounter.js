6374e1aa13e489d886d0ad6f5921ecb0f214f569
nomanme <abdullah.noman083@gmail.com>
2021-09-07 00:18:16 +0600

    movieDisplay component add

--- 33-how-react-works-recap-react-concepts/recap-react-concepts/src/MovieCounter.js
+++ 33-how-react-works-recap-react-concepts/recap-react-concepts/src/MovieCounter.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import MovieDisplay from './MovieDisplay';
 
 const MovieCounter = () => {
 	// const result = ['abc', 'def'];
@@ -7,13 +8,32 @@ const MovieCounter = () => {
 	// const secondOne = result[1];
 
 	// console.log(result, first, second, firstOne, secondOne);
-
 	const [count, setCount] = useState(0);
-	console.log(count);
+
+	const handleAddMovie = () => {
+		return setCount(count + 1);
+	};
+	const handleRemoveMovie = () => {
+		if (count === 0) {
+			return;
+		} else {
+			setCount(count - 1);
+		}
+	};
+
 	return (
 		<div>
-			<button className="btn btn-info">Add Movie</button>
+			<button onClick={handleRemoveMovie} className="btn btn-danger m-2">
+				Remove Movie
+			</button>
+			<button onClick={handleAddMovie} className="btn btn-info m-2">
+				Add Movie
+			</button>
+
 			<h5>Number of Movies: {count} </h5>
+			<MovieDisplay movies={count + 3}></MovieDisplay>
+			<MovieDisplay movies={count + 5}></MovieDisplay>
+			<MovieDisplay movies={count + 10}></MovieDisplay>
 		</div>
 	);
 };
