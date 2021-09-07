92149388c097ff0b96dcbbf1899654595e2d8aed
nomanme <abdullah.noman083@gmail.com>
2021-09-08 00:41:45 +0600

    display products details with photo

--- 34-simple-react-spa-with-multiple-components/ema-john-simple/src/components/Product/Product.js
+++ 34-simple-react-spa-with-multiple-components/ema-john-simple/src/components/Product/Product.js
@@ -2,12 +2,24 @@ import React from 'react';
 import './Product.css';
 
 const Product = (props) => {
-	// console.log(props.product);
-	const { name, category, img, price, shipping } = props.product;
+	console.log(props.product);
+	const { name, seller, stock, category, img, price, shipping } = props.product;
 	return (
-		<div>
-			<h6>{name}</h6>
-		</div>
+		<section className="product">
+			<div className="itemsPhoto">
+				<img src={img} alt="" />
+			</div>
+			<div className="productItem">
+				<h6 className="productName">{name}</h6>
+				<p>
+					<small>by: {seller}</small>
+				</p>
+				<h6>${price}</h6>
+				<p>
+					<small>Only {stock} left in stock - Order Soon</small>
+				</p>
+			</div>
+		</section>
 	);
 };
 
