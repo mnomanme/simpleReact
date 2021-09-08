4810a2e7b6ab4f147d9c00e5dc49accbcb623670
nomanme <abdullah.noman083@gmail.com>
2021-09-09 02:43:28 +0600

    pass country state

--- 34.5-simple-react-spa-practice/world-tour/src/components/Country/Country.js
+++ 34.5-simple-react-spa-practice/world-tour/src/components/Country/Country.js
@@ -1,56 +1,14 @@
-import React, { useEffect, useState } from 'react';
-import { Card, CardGroup, Container, Row } from 'react-bootstrap';
+import React from 'react';
+import CountryState from '../CountryState/CountryState';
 import './Country.css';
 
 const Country = () => {
+	const handleCountryAdd = (country) => console.log('add country', country);
+
 	return (
 		<div>
-			<CountryState></CountryState>
+			<CountryState handleCountryAdd={handleCountryAdd} />
 		</div>
-	);
-};
-
-const CountryState = () => {
-	const [countries, setCountries] = useState([]);
-
-	useEffect(() => {
-		const url = `https://restcountries.eu/rest/v2/all`;
-
-		// load data
-		const fetchData = async () => {
-			try {
-				const res = await fetch(url);
-				const data = await res.json();
-				// console.log(data);
-				setCountries(data);
-			} catch (error) {
-				console.log(error);
-			}
-		};
-		fetchData();
-	}, []);
-
-	return (
-		<Container>
-			<h2>Country Load: {countries.length}</h2>
-			<Row xs={1} md={2} className="g-4 ">
-				{countries.map((country) => {
-					const { name, flag, alpha3Code } = country;
-					// console.log(name);
-					return (
-						<CardGroup className="cardGroup" key={alpha3Code}>
-							<Card className="cardHover">
-								<Card.Img variant="top" src={flag} />
-								<Card.Body>
-									<Card.Title>{name}</Card.Title>
-									<Card.Text>This is a wider card</Card.Text>
-								</Card.Body>
-							</Card>
-						</CardGroup>
-					);
-				})}
-			</Row>
-		</Container>
 	);
 };
 
