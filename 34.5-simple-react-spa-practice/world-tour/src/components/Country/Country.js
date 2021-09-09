e4bb7d25ffd869b9bc386c1ba2880c49a3c2d512
nomanme <abdullah.noman083@gmail.com>
2021-09-09 12:00:38 +0600

    remove

--- 34.5-simple-react-spa-practice/world-tour/src/components/Country/Country.js
+++ 34.5-simple-react-spa-practice/world-tour/src/components/Country/Country.js
@@ -1,13 +1,10 @@
-import React from 'react';
 import CountryState from '../CountryState/CountryState';
 import './Country.css';
 
 const Country = () => {
-	const handleCountryAdd = (country) => console.log('add country', country);
-
 	return (
 		<div>
-			<CountryState handleCountryAdd={handleCountryAdd} />
+			<CountryState />
 		</div>
 	);
 };
