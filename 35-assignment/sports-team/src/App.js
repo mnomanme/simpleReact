ae05372ac360b61e522fdbb8a36c8037e3abdd39
nomanme <abdullah.noman083@gmail.com>
2021-09-10 19:20:38 +0600

    layout add

--- 35-assignment/sports-team/src/App.js
+++ 35-assignment/sports-team/src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import { Container } from 'react-bootstrap';
+import Layout from './components/Layout/Layout';
 
 const App = () => {
 	return (
-		<section className="App">
-			<h2>Hello Sports Team</h2>
-		</section>
+		<Container>
+			<section className="App bg-info">
+				<h2>Hello Sports Team</h2>
+			</section>
+			<Layout />
+		</Container>
 	);
 };
 
