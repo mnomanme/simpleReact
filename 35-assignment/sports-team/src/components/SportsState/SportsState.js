0346c4e4a2855aad442fb48304923cccb20a144d
nomanme <abdullah.noman083@gmail.com>
2021-09-10 20:06:18 +0600

    load & show data

--- 35-assignment/sports-team/src/components/SportsState/SportsState.js
+++ 35-assignment/sports-team/src/components/SportsState/SportsState.js
@@ -1,10 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
+import SportsData from '../../resources/FakeSportsData/SportsData';
 
 const SportsState = () => {
+	// console.log(SportsData);
+
 	return (
 		<div>
 			<h2>Sports State</h2>
+			<TeamData />
 		</div>
+	);
+};
+
+const TeamData = () => {
+	const teamSports = SportsData;
+	console.log(teamSports);
+
+	const [team, setTeam] = useState(teamSports);
+
+	return (
+		<section>
+			{team.map((t) => {
+				const { first_name, last_name, playerPhoto } = t;
+				return (
+					<>
+						<div>
+							<img src={playerPhoto} alt="" style={{ height: '100px' }} />
+						</div>
+						<li>
+							{first_name}
+							{last_name}
+						</li>
+					</>
+				);
+			})}
+		</section>
 	);
 };
 
