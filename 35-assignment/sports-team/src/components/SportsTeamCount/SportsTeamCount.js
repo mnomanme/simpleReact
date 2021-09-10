afdee48c926de0d27ac7f5db56ba3908973fffd0
nomanme <abdullah.noman083@gmail.com>
2021-09-10 22:29:14 +0600

    count player salary & with name also salary

--- 35-assignment/sports-team/src/components/SportsTeamCount/SportsTeamCount.js
+++ 35-assignment/sports-team/src/components/SportsTeamCount/SportsTeamCount.js
@@ -1,9 +1,30 @@
 import React from 'react';
 
-const SportsTeamCount = () => {
+const SportsTeamCount = (props) => {
+	// console.log(props);
+	const addPlayer = props.player;
+	// console.log(addPlayer);
+	const playerTotalSalary = addPlayer.reduce((salary, team) => salary + team.salary, 0);
+
 	return (
 		<div>
-			<h2>Sport Team Count</h2>
+			{addPlayer.map((team) => {
+				console.log('name', team);
+				const { first_name, last_name, salary, id } = team;
+				return (
+					<section key={id}>
+						<h5>
+							Name: {first_name} {}
+							{last_name} Demand: {`$`}
+							{salary} {`million`}
+						</h5>
+					</section>
+				);
+			})}
+			<h4>
+				Total Player Salary:{`$`}
+				{playerTotalSalary} {`million`}
+			</h4>
 		</div>
 	);
 };
