bee7d529225284e81d985d9e611f7a6ef6d138bf
nomanme <abdullah.noman083@gmail.com>
2021-09-10 20:09:07 +0600

    pass sports

--- 35-assignment/sports-team/src/components/Layout/Layout.js
+++ 35-assignment/sports-team/src/components/Layout/Layout.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Sports from '../Sports/Sports';
 
 const Layout = () => {
 	return (
 		<div>
 			<h2>Layout</h2>
+			<Sports />
 		</div>
 	);
 };
--- 35-assignment/sports-team/src/components/Sports/Sports.js
+++ 35-assignment/sports-team/src/components/Sports/Sports.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import SportsState from '../SportsState/SportsState';
 
 const Sports = () => {
 	return (
 		<div>
 			<h2>Sports</h2>
+			<SportsState />
 		</div>
 	);
 };
