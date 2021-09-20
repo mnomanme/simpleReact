0907964e8c2d925955b81417389d06604752e631
nomanme <abdullah.noman083@gmail.com>
2021-09-20 21:44:47 +0600

    col add

--- 35-assignment/sports-team/src/components/SportsState/SportsState.js
+++ 35-assignment/sports-team/src/components/SportsState/SportsState.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import SportsData from '../../resources/FakeSportsData/SportsData';
-import { Card, CardGroup, Row, Button } from 'react-bootstrap';
+import { Card, CardGroup, Row, Button, Col } from 'react-bootstrap';
 import SportsTeamCount from '../SportsTeamCount/SportsTeamCount';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCertificate, faIdBadge } from '@fortawesome/free-solid-svg-icons';
@@ -39,35 +39,37 @@ const TeamData = () => {
 				<h4>Add Team: {player.length}</h4>
 				<SportsTeamCount player={player} />
 			</div>
-			<Row xs={1} md={2} className="g-4 ">
+			<Row xs={1} md={3} className="g-4 ">
 				{team.map((player) => {
 					const { first_name, last_name, playerPhoto, id, position, dateOfBirth, salary } = player;
 					return (
-						<CardGroup key={id}>
-							<Card>
-								<Card.Img variant="top" src={playerPhoto} style={{ height: '400px' }} />
-								<Card.Body className="text-center">
-									<Card.Title>
-										{first_name}
-										{last_name} {`	`}
-										<FontAwesomeIcon icon={faCertificate} />
-									</Card.Title>
-									<Card>
-										<small>Position: {position} </small>
-										<small>Date of Birth: {dateOfBirth} </small>
-										<small>
-											Salary: {`$`}
-											{salary}
-											{` `}
-											{`million`}
-										</small>
-									</Card>
-								</Card.Body>
-								<Button onClick={() => handleAddPlayer(player)} variant="info">
-									<FontAwesomeIcon icon={faIdBadge} /> Add Player
-								</Button>
-							</Card>
-						</CardGroup>
+						<Col>
+							<CardGroup key={id}>
+								<Card>
+									<Card.Img variant="top" src={playerPhoto} style={{ height: '400px' }} />
+									<Card.Body className="text-center">
+										<Card.Title>
+											{first_name}
+											{last_name} {`	`}
+											<FontAwesomeIcon icon={faCertificate} />
+										</Card.Title>
+										<Card>
+											<small>Position: {position} </small>
+											<small>Date of Birth: {dateOfBirth} </small>
+											<small>
+												Salary: {`$`}
+												{salary}
+												{` `}
+												{`million`}
+											</small>
+										</Card>
+									</Card.Body>
+									<Button onClick={() => handleAddPlayer(player)} variant="info">
+										<FontAwesomeIcon icon={faIdBadge} /> Add Player
+									</Button>
+								</Card>
+							</CardGroup>
+						</Col>
 					);
 				})}
 			</Row>
